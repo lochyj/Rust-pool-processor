c20adea9d6716651f0b0ab84e5ff16f566050563
Lachlanjowett <79903111+lochyj@users.noreply.github.com>
2022-10-10 10:53:48 +1100

    created a heatmap generator that singles out the large sections of white

--- src/main.rs
+++ src/main.rs
@@ -3,66 +3,42 @@ include!("./util.rs");
 use image::{ImageBuffer, Luma, GenericImageView};
 
 fn main() {
+    // Load the image from file
     let img = image::open("src/images/formattedTable.jpg").unwrap();
-
+    // Generate the grayscale image of var img
     let gray_scale = img_to_gray_scale(&img);
 
+    // Define heat_map image and the new_image
+    let mut heat_image: ImageBuffer<_, Vec<_>> = ImageBuffer::new(gray_scale.width(), gray_scale.height());
     let mut new_image: ImageBuffer<_, Vec<_>> = ImageBuffer::new(gray_scale.width(), gray_scale.height());
 
     for (x, y, pixel_luminocity) in gray_scale.enumerate_pixels() {
-        let white_threshold: u8 = 210;
+        let white_threshold: u8 = 190;
         if pixel_luminocity[0] > white_threshold {
             new_image.put_pixel(x, y, image::Luma([255u8]));
+            
+        }
+        let white_threshold2: u8 = 255;
+        if pixel_luminocity[0] == white_threshold2 {
+            heat_image.put_pixel(x, y, image::Luma([255u8]));
         }
     }
 
-    let balls = ImageBuffer::new(new_image.width(), new_image.height());
+    println!("Created black and white image");
 
-    let ball_locations = return_ball_locations(&new_image);
+    let ball_locations = return_heatmap(&new_image);
 
-    for i in 0..ball_locations.len() {
-        println!("Ball {} / {} is at: {:?}", i,ball_locations.len(), ball_locations[i]);
-        if ball_locations[i] == (1627, 1372) {
-            break;
-        }
-    }
     println!("{}", ball_locations.len());
 
+    for i in 0..ball_locations.len() {
+        heat_image.put_pixel(ball_locations[i].0, ball_locations[i].1, image::Luma([(ball_locations[i].2).try_into().unwrap()]));
+    }
+
+    heat_image.save("src/images/heat_image.jpg").unwrap();
     new_image.save("src/images/grayScale.jpg").unwrap();
 }
 
-fn return_ball_locations(img: &ImageBuffer<Luma<u8>, Vec<u8>>) -> Vec<(u32, u32)> {
-    let mut ball_locations: Vec<(u32, u32)> = Vec::new();
-    let white: u8 = 255;
-    let luma_white: Luma<u8> = image::Luma([255u8]);
 
-    for (x, y, pixel_luminocity) in img.enumerate_pixels() {
-        
-        let mut white_loc: Vec<(u32, u32)> = Vec::new();
-
-        if pixel_luminocity[0] < white {
-
-            if (x>5 && y>5) && (x<img.width()-5 && y<img.height()-5) {
-                let mut white_is:bool = false;
-                for i in 0..5 {
-                    for j in 0..5 {
-                        if img.get_pixel(x+i, y+j) == &luma_white {
-                            white_is = true;
-                        }
-                    }
-                    
-                }
-                if white_is == true {
-                    //white_loc.push((x, y));
-                    ball_locations.push((x, y));
-                }
-                
-            }
-        }
-    }
-
-    return ball_locations;
-}
 
 
 
--- src/util.rs
+++ src/util.rs
@@ -6,3 +6,52 @@
 fn img_to_gray_scale(img: &image::DynamicImage) -> image::GrayImage {
     return image::imageops::grayscale(img);
 }
+
+/*
+ * Returns the heatmap of the image
+ */
+
+fn return_heatmap(img: &ImageBuffer<Luma<u8>, Vec<u8>>) -> Vec<(u32, u32, i32)> {
+    let mut ball_locations: Vec<(u32, u32, i32)> = Vec::new();
+    let white: u8 = 255;
+    let luma_white: Luma<u8> = image::Luma([255u8]);
+
+    println!("Starting scan");
+
+    // X
+    let x_scan: i32 = 50;
+    let x_scan_iter: u32 = 50;
+
+    // Y
+    let y_scan: i32 = 50;
+    let y_scan_iter: u32 = 50;
+
+    for (x, y, pixel_luminocity) in img.enumerate_pixels() {
+
+        if pixel_luminocity[0] == white {
+
+            let mut white_num: i32 = 0;
+            
+            for i in 0..x_scan_iter {
+                for j in 0..y_scan_iter {
+                    if (x>i && y >j) &&  (x<img.width()-i && y<img.height()-j) {
+                        if img.get_pixel(x+i, y+j) == &luma_white {
+                            white_num += 1;
+                        }
+                        if img.get_pixel(x-i, y-j) == &luma_white {
+                            white_num += 1;
+                        }
+                    } 
+                }
+                
+                ball_locations.push((x, y, (white_num* 5 / (x_scan + y_scan))));
+                
+            }
+
+        }
+    }
+
+    println!("Scan returned");
+
+    return ball_locations;
+}

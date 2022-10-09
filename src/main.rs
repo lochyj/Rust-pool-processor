0115d92cdeea89ce62964d4337790f463f9e2be4
Lachlan Jowett <lochyjowett1@gmail.com>
2022-10-09 20:38:28 +1100

    Working on singling out the pool balls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 include!("./util.rs");
 
-use image::{ImageBuffer, Luma};
+use image::{ImageBuffer, Luma, GenericImageView};
 
 fn main() {
     let img = image::open("src/images/formattedTable.jpg").unwrap();
@@ -16,8 +16,16 @@ fn main() {
         }
     }
 
+    let balls = ImageBuffer::new(new_image.width(), new_image.height());
+
     let ball_locations = return_ball_locations(&new_image);
 
+    for i in 0..ball_locations.len() {
+        println!("Ball {} / {} is at: {:?}", i,ball_locations.len(), ball_locations[i]);
+        if ball_locations[i] == (1627, 1372) {
+            break;
+        }
+    }
     println!("{}", ball_locations.len());
 
     new_image.save("src/images/grayScale.jpg").unwrap();
@@ -25,11 +33,31 @@ fn main() {
 
 fn return_ball_locations(img: &ImageBuffer<Luma<u8>, Vec<u8>>) -> Vec<(u32, u32)> {
     let mut ball_locations: Vec<(u32, u32)> = Vec::new();
+    let white: u8 = 255;
+    let luma_white: Luma<u8> = image::Luma([255u8]);
 
     for (x, y, pixel_luminocity) in img.enumerate_pixels() {
-        let white: u8 = 255;
+        
+        let mut white_loc: Vec<(u32, u32)> = Vec::new();
+
         if pixel_luminocity[0] < white {
-            ball_locations.push((x, y));
+
+            if (x>5 && y>5) && (x<img.width()-5 && y<img.height()-5) {
+                let mut white_is:bool = false;
+                for i in 0..5 {
+                    for j in 0..5 {
+                        if img.get_pixel(x+i, y+j) == &luma_white {
+                            white_is = true;
+                        }
+                    }
+                    
+                }
+                if white_is == true {
+                    //white_loc.push((x, y));
+                    ball_locations.push((x, y));
+                }
+                
+            }
         }
     }
 
